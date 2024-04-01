e32483af0576bee309eedcc92948c236bff05853
stray-code <abc1007020@gmail.com>
2024-04-01 21:58:12 +0900

    crxjs追加と設定

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,15 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { crx, defineManifest } from '@crxjs/vite-plugin'
+
+const manifest = defineManifest({
+    manifest_version: 3,
+    name: '自動ページ更新',
+    version: '1.0.0',
+    action: { default_popup: 'index.html' }
+})
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), crx({ manifest }),],
 })
