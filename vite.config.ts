de0d3c2476a032c87dc8b7e84a1186113140889a
stray-code <abc1007020@gmail.com>
2024-04-02 20:52:25 +0900

    manifest修正

--- vite.config.ts
+++ vite.config.ts
@@ -3,13 +3,39 @@ import react from '@vitejs/plugin-react'
 import { crx, defineManifest } from '@crxjs/vite-plugin'
 
 const manifest = defineManifest({
-    manifest_version: 3,
-    name: '自動ページ更新',
-    version: '1.0.0',
-    action: { default_popup: 'index.html' }
+  manifest_version: 3,
+  name: '自動ページ更新（リロード）',
+  version: '1.0.0',
+  icons: {
+    16: 'src/assets/img/icon16.png',
+    48: 'src/assets/img/icon48.png',
+    128: 'src/assets/img/icon128.png'
+  },
+  action: {
+    default_icon: 'src/assets/img/icon16.png',
+    default_popup: 'src/popup/index.html',
+  },
+  content_scripts: [
+    {
+      js: ['src/content/main.ts'],
+      matches: ['http://*/*', 'https://*/*'],
+    }
+  ],
+  background: {
+    service_worker: "src/background.ts",
+    type: "module"
+  },
+  permissions: ["storage"],
 })
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react(), crx({ manifest }),],
+  server: {
+    port: 5173,
+    strictPort: true,
+    hmr: {
+      port: 5173,
+    },
+  },
 })
