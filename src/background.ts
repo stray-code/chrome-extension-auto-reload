5ba88eb910e8760818496d3c653bb9c532d90ebc
stray-code <abc1007020@gmail.com>
2024-04-21 21:11:09 +0900

    タブを閉じた時にストレージのtabIdを0に

--- src/background.ts
+++ src/background.ts
@@ -11,3 +11,25 @@ chrome.runtime.onMessage.addListener((message, sender, response) => {
     response(sender.tab.id);
   }
 });
+
+chrome.tabs.onRemoved.addListener((tabId) => {
+  chrome.storage.local.get(["AUTO_RELOAD"], async (value) => {
+    if (!value?.AUTO_RELOAD) {
+      return;
+    }
+
+    if (value.AUTO_RELOAD.tabId !== tabId) {
+      return;
+    }
+
+    chrome.storage.local.set({
+      AUTO_RELOAD: {
+        tabId: 0,
+        minutes: value.AUTO_RELOAD.minutes,
+        seconds: value.AUTO_RELOAD.seconds,
+      },
+    });
+  });
+
+  return;
+});
